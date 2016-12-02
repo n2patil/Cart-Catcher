6ea4dccbaecc0c39785f312ab25dcd06066877fc
n2patil <n2patil@UCSD.EDU>
2016-12-01 17:25:49 -0800

    Added changing text.

---
+++ Assets/TextOptions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextOptions : MonoBehaviour {
+
+    public Text textUI;
+    private int textIndex = 0;
+
+
+    public static string[] questions = new string[]
+    {
+        "Pick 3 items that are a good source of Vitamin A.",
+        "Pick 2 items that are a high source of fiber.",
+        "Pick 3 items that are high source of protein."
+    };
+
+    public void Start()
+    {
+
+        UpdateText();
+
+
+    }
+
+    public void CycleOptions()
+    {
+
+        textIndex++;
+
+        if (textIndex >= questions.Length)
+        {
+
+            textIndex = 0;
+
+        }
+
+        UpdateText();
+
+    }
+
+    public void UpdateText()
+    {
+
+        textUI.text = questions[textIndex];
+
+    }
+}
