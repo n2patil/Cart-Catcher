91ff7649a3d7f2de5ad9317c4cb21bb288f9ff3d
Namitha Patil <nami.patil12@gmail.com>
2016-12-04 16:46:17 -0800

    question change working

--- Assets/Cart.cs
+++ Assets/Cart.cs
@@ -1,10 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Cart : MonoBehaviour {
 
+	public static TextOptions.FoodRequirement currentRequirement;
+
+	public TextOptions.FoodRequirement currentFood;
+
+	public List<Food> activeFood;
+
+	public Text foodText;
+
+
 	// Use this for initialization
 	void Start () {
+		currentFood = new TextOptions.FoodRequirement ();
+		activeFood = new List<Food> ();
+
+		foodText.text = currentFood.ToString ();
+
 	
 	}
 	
@@ -14,12 +30,90 @@ public class Cart : MonoBehaviour {
 	}
 
     public void OnTriggerEnter2D(Collider2D other)
-    {
+	{
+
+		Food fallenFood = other.GetComponent<Food> ();
+
+		if (fallenFood != null) {
+
+			Destroy (fallenFood.GetComponent<Rigidbody2D> ());
+			Destroy (fallenFood.GetComponent<Collider2D> ());
+
+			activeFood.Add (fallenFood);
+
+			fallenFood.transform.localScale *= 0.5f;
+
+			if (fallenFood.hasCalcium) {
+
+				currentFood.numCalcium++;
+
+			}
+
+			if (fallenFood.hasFiber) {
+
+				currentFood.numFiber++;
+
+			}
+
+			if (fallenFood.hasVitaminA) {
+
+				currentFood.numVitaminA++;
+				Debug.LogFormat ("Num of vitA is {0}, need {1}", currentFood.numVitaminA, currentRequirement.numVitaminA);
+
+			}
 
 
-        Destroy(other.gameObject);
 
+			CheckRequirement ();
+
+			foodText.text = currentFood.ToString ();
+
+		}
+
+
+		//Destroy(other.gameObject);
 
 
     }
+
+	public void CheckRequirement() {
+
+		if (currentFood.numCalcium < currentRequirement.numCalcium) {
+			Debug.Log("Not enough calcium");
+			return;
+		}
+
+		if (currentFood.numFiber < currentRequirement.numFiber){
+			Debug.Log("Not enough fiber");
+
+			return;
+		}
+
+		if (currentFood.numVitaminA < currentRequirement.numVitaminA){
+			Debug.Log("Not enough vitamin A");
+
+			return;
+		}
+
+		TextOptions.instance.CycleOptions ();
+
+		ClearCart ();
+
+
+	}
+
+	public void ClearCart() {
+
+		foreach (Food food in activeFood) {
+
+			GameObject.Destroy (food.gameObject);
+
+		}
+
+		activeFood.Clear ();
+
+		currentFood = new TextOptions.FoodRequirement ();
+		foodText.text = currentFood.ToString ();
+
+	}
 }
--- Assets/TextOptions.cs
+++ Assets/TextOptions.cs
@@ -1,24 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class TextOptions : MonoBehaviour {
+
+	public static TextOptions instance;
 
     public Text textUI;
     private int textIndex = 0;
 
+	public struct FoodRequirement {
 
-    public static string[] questions = new string[]
+		public int numFiber;
+		public int numVitaminA;
+		public int numCalcium;
+
+		public FoodRequirement(int fiber, int vitA, int calcium) {
+
+			numFiber = fiber;
+			numVitaminA = vitA;
+			numCalcium = calcium;
+
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("Fiber: {0} \n Vitamin A: {1} \n Calcium: {2}", numFiber, numVitaminA, numCalcium);
+		}
+	}
+
+	public static KeyValuePair<string, FoodRequirement>[] questions = new KeyValuePair<string, FoodRequirement>[]
     {
-        "Pick 3 items that are a good source of Vitamin A.",
-        "Pick 2 items that are a high source of fiber.",
-        "Pick 3 items that are high source of protein."
+		new KeyValuePair<string, FoodRequirement> ("Pick 3 items that are a good source of Vitamin A.", new FoodRequirement(0, 3, 0)),
+		new KeyValuePair<string, FoodRequirement> ("Pick 2 items that are a high source of fiber.", new FoodRequirement(2,0,0)),
+		new KeyValuePair<string, FoodRequirement> ("Pick 3 items that are high source of calcium.", new FoodRequirement(0,0,3))
     };
+
+	public void Awake() {
+
+		if (instance == null) {
+			instance = this;
+		}
+
+
+	}
 
     public void Start()
     {
 
-        UpdateText();
+		UpdateRequirement ();
 
     }
 
@@ -34,14 +65,20 @@ public class TextOptions : MonoBehaviour {
 
         }
 
-        UpdateText();
+		UpdateRequirement ();
+
+
 
     }
 
-    public void UpdateText()
+    public void UpdateRequirement()
     {
 
-        textUI.text = questions[textIndex];
+		Cart.currentRequirement = questions [textIndex].Value;
+
+		Debug.Log ("Current requirement: " + Cart.currentRequirement.ToString ());
+
+        textUI.text = questions[textIndex].Key;
 
     }
 }
